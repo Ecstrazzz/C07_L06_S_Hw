fbb6a4ea58fd05779657bc6bb847571a99de13e1
Ecs <ecstrazzz@ya.ru>
2023-12-27 01:08:44 +0500

    added the first homework assignment

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Знакомство с языками программирования (семинары)
+
+// Урок 6. Массивы и строки
+
+// Задача 1: Задайте двумерный массив символов
+// (тип char [,]). Создать строку из символов этого массива.
+
+// Пример: a b c => “abcdef”
+//         d e f
+
+// Решение:
+
+char[,] symbols = new char[2, 3]
+{
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'}
+};
+
+string GetStringFromArray()
+{
+    string str = "";
+    for (int i = 0; i < symbols.GetLength(0); i++)
+    {
+        for (int j = 0; j < symbols.GetLength(1); j++)
+        {
+            str += symbols[i, j];
+        }
+    }
+    return str;
+}
+
+Console.WriteLine(GetStringFromArray());
